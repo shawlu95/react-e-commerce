7a289f071ea8d060bd16c4144d0dcb009f0e7bef
Shaw Lu <shawlu95@gmail.com>
2022-07-08 08:18:25 -0700

    Setup products context

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,6 +9,7 @@ import CartButtons from './CartButtons';
 import { useUserContext } from '../context/user_context';
 
 const Sidebar = () => {
+  const { isSidebarOpen, openSidebar, closeSidebar } = useProductsContext();
   const isOpen = true;
   return (
     <SidebarContainer>
