9140d6f18bc62bb7cf049f3187a20bee86c15ec7
Shaw Lu <shawlu95@gmail.com>
2022-07-08 17:13:23 -0700

    Display product details page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -16,6 +16,7 @@ import { Link } from 'react-router-dom';
 
 const SingleProductPage = () => {
   const { id } = useParams();
+  const history = useHistory();
   const {
     loadingSingleProduct: loading,
     loadingSingleProductError: error,
@@ -26,8 +27,69 @@ const SingleProductPage = () => {
   useEffect(() => {
     fetchProduct(`${url}${id}`);
   }, []);
-  console.log(product);
-  return <h4>single product page</h4>;
+
+  useEffect(() => {
+    if (error) {
+      // back to home page after 3 seconds
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
+    }
+  }, [error]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product;
+
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className='section section-center'>
+        <Link to='/products' className='btn'>
+          Back to Products
+        </Link>
+        <div className='products-center'>
+          <ProductImages />
+          <section className='content'>
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className='price'>{formatPrice(price)}</h5>
+            <p className='desc'>{description}</p>
+            <p className='info'>
+              <span>Available : </span>
+              {stock > 0 ? 'In Stock' : 'Out of Stock'}
+            </p>
+            <p className='info'>
+              <span>SKU : </span>
+              {sku}
+            </p>
+            <p className='info'>
+              <span>Brand : </span>
+              {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.main`
